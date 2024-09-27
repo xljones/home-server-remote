768cd364f05fc352c37c5e23f9dab97493c849a1
Xander Jones <xander.jones@tails.com>
2024-09-27 11:11:45 +0100

    feat: restore main bp

--- service/views/__init__.py
+++ service/views/__init__.py
@@ -1,3 +1,4 @@
 from flask import Blueprint
+from service.views.main import bp as main_bp
 
-VIEW_BLUEPRINTS: list[Blueprint] = []
+VIEW_BLUEPRINTS: list[Blueprint] = [main_bp]
--- service/views/main.py
+++ service/views/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 bp = Blueprint("main", __name__, url_prefix="")
 
@@ -8,3 +8,8 @@ def home() -> str:
     return render_template(
         "index.html",
     )
+
+
+@bp.get("/ping")
+def ping() -> tuple[str, int]:
+    return jsonify({"message": "pong"}), 200
